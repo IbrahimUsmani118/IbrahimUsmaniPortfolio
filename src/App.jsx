7bf3159bdb2d6cc27169853da9341fa6ddedd873
Ibrahim Usmani <78166163+IbrahimUsmani118@users.noreply.github.com>
2023-12-04 14:45:52 +0000

    Update based on Resume and some additions

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,115 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
+        <h1>Ibrahim Usmani</h1>
+        <p>Piscataway, New Jersey, United States</p>
+        <p>Email: onlyibrahim13@gmail.com</p>
+        <p>Phone: (732) 890-8125</p>
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.linkedin.com/in/ibrahim-usmani/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            LinkedIn
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://github.com/IbrahimUsmani118"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
           </a>
         </p>
       </header>
+      <section>
+  <h2>SUMMARY</h2>
+  <p>
+    Experienced Full Stack Engineer skilled in Python Development, NoSQL, SQL, object-oriented programming, optimizing performance, and maintaining design. Proficient in test-driven development, cross-functional projects, and data platforms.
+  </p>
+    </section>
+
+    <section>
+      <h2>EXPERIENCE</h2>
+      <article>
+        <h3>Data Analyst/Software Engineer</h3>
+        <p>New Jersey Parent Caucus May 2023 - Present, US, NJ, Elizabeth</p>
+        <ul>
+          <li>Aligned server enhancements with project requirements, reducing latency by 30% during user story definition, ensuring improved Overall Organization.</li>
+          <li>Adhered to coding standards, boosting data throughput by 40% for a Data Aligned Project, benefiting the Internal Team.</li>
+          <li>Implemented stringent data encryption, reducing potential security vulnerabilities by 95%, covering Multiple Data Sources.</li>
+          <li>Engineered scalable architecture, achieving a 25% latency decrease, meeting data requirements and adhering to SDLC standards.</li>
+          <li>Executed API-based solutions, reducing latency by 20% and optimizing application performance for dashboarding, enhancing compatibility with diverse Database Platforms.</li>
+        </ul>
+      </article>
+      
+      <article>
+        <h3>Semantic Engineer</h3>
+        <p>TargetArc June 2023 - September 2023, NJ</p>
+        <ul>
+          <li>Spearheaded Node.js and Express.js app development, ensuring adherence to Multiple System considerations and Data Conversions, yielding a notable 25% increase in application responsiveness.</li>
+          <li>Achieved 90% stakeholder satisfaction by prioritizing requirements, implementing Client Implementation strategies, and providing Data Reports, resulting in an 80% decrease in bug resolution time, significantly meeting user needs.</li>
+          <li>Implemented JavaScript optimization, including Data Migrations and File Formats enhancement, reducing bug resolution time by 40% while meeting coding standards and addressing user needs.</li>
+          <li>Established robust database systems for Node.js and Express.js applications, considering Mapping Client aspects and File Formats, resulting in a 20% decrease in response latency, crucial for efficient data projects.</li>
+          <li>Integrated API-driven solutions with Agile and Scrum methodologies, incorporating Source Data and Data Conversions, resulting in a substantial 30% increase in system flexibility and responsiveness for diverse projects.</li>
+        </ul>
+      </article>
+    </section>
+
+    <section>
+      <h2>SPECIALIZATIONS</h2>
+      <ul>
+          <li>Full Stack Development: Creating robust applications from scratch using Python, JavaScript, SQL, and object-oriented programming.</li>
+          <li>Database Management: Optimizing performance, securing data, and designing scalable architecture for efficient database handling.</li>
+          <li>Test-Driven Development: Building high-quality software with a focus on error-free code through rigorous testing.</li>
+          <li>Cross-Functional Projects: Seamlessly collaborating across teams for successful project completion.</li>
+          <li>API-Based Solutions: Improving system efficiency by implementing API-based enhancements and automation.</li>
+          <li>Project Management: Leading projects, ensuring stakeholder satisfaction, and accurate project management.</li>
+          <li>JavaScript Optimization: Enhancing system performance by optimizing JavaScript functions and database maintenance.</li>
+          <li>Data Analysis and Reporting: Analyzing data, building models, and creating reports to derive actionable insights.</li>
+          <li>Educational Services: Providing tutoring or educational sessions in Computer Science and Data Science.</li>
+          <li>Skill Training and Workshops: Conducting workshops on SQL, Excel, Tableau, ETL Processes, Python, and R.</li>
+          <li>Consultancy Services: Advising on database design, data modeling, and effective data analysis techniques.</li>
+          <li>Data-Driven Decision Making: Assisting in monitoring data, identifying trends, and offering data-driven recommendations.</li>
+      </ul>
+    </section>
+
+    <section>
+      <h2>PROJECTS</h2>
+      <article>
+        <h3>CPU Scheduling Algorithms</h3>
+        <ul>
+          <li>This Java project showcases my skills in Operating Systems, particularly in CPU Scheduling Algorithms like First Come First Serve, Round Robin, and Shortest Job First. It's a testament to my expertise in this area, and I value the confidentiality of my work. Please respect that reproduction without explicit permission is prohibited. I continuously enhance this codebase to integrate new knowledge and improvements, ensuring its ongoing relevance and refinement.</li>
+        </ul>
+      </article>
+      
+      <article>
+        <h3>Web Browser Utility-iOS</h3>
+        <ul>
+          <li>Quick Web Hub is your personal web portal on the go. Seamlessly access Google, YouTube, Netflix, and more with a simple tap. Say goodbye to app switching – enjoy smooth browsing as your favorite sites come together in one convenient spot! It's your gateway to instant access, simplifying your online experience.</li>
+        </ul>
+      </article>
+      
+      <article>
+        <h3>HealthCare Management System</h3>
+        <ul>
+          <li>Healthcare-Management-System is a SQL-driven project designed for portfolio demonstration, showcasing efficient healthcare data management, including appointment scheduling, patient management, and medical records. Its codebase encompasses crucial files like Appointment.java, DatabaseDataInsertion.java, DatabaseSchemaConnection.java, Main.java, MedicalRecord.java, and Patient.java</li>
+        </ul>
+      </article>
+      
+      <article>
+        <h3>JS Data Generator</h3>
+        <ul>
+          <li>The JS Data Generator for MongoDB stands as a powerful utility within my portfolio, facilitating seamless data generation and storage in MongoDB using JavaScript. This project not only simplifies data importation from CSV files into MongoDB but also serves as an invaluable tool for tasks like data migration and seeding, showcasing my adeptness in database management and data manipulation.</li>
+        </ul>
+      </article>
+    </section>
+
     </div>
-  );
+    );
 }
 
 export default App;
